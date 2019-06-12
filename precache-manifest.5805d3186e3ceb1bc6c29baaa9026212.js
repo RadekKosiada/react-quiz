0b7f503482f81691458da174e91c46d680b01b43
RadekKosiada <kosiadar@gmail.com>
2019-06-12 13:39:54 +0200

    Updates

---
+++ precache-manifest.5805d3186e3ceb1bc6c29baaa9026212.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebad5c2f719c0ba2711dca6a704b860b",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "149f92b5abbf3a6f1adb",
+    "url": "/react-quiz/static/css/main.ce7aa88f.chunk.css"
+  },
+  {
+    "revision": "b2780629e3be9048813c",
+    "url": "/react-quiz/static/js/2.9dfab21d.chunk.js"
+  },
+  {
+    "revision": "149f92b5abbf3a6f1adb",
+    "url": "/react-quiz/static/js/main.4ba124e3.chunk.js"
+  },
+  {
+    "revision": "260e1d56f90ee7d17fb2",
+    "url": "/react-quiz/static/js/runtime~main.009b0ca2.js"
+  }
+]);
